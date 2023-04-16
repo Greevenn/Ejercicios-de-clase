3b9d8c0a1b587a77a5a52a675c41b25d88d78df4
Greevenn <stevengreyvin00@gmail.com>
2023-04-16 17:22:11 -0600

    actualizacion listos

---
+++ ejercicio32.js
@@ -0,0 +1,3 @@
+//32.Dado un arreglo de números, escribe una función que devuelva la suma de
+//todos los números pares. Si el arreglo está vacío o no contiene números 
+//pares, la función deberá devolver 0.
